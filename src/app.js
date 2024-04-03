5dce7658866684f40703ed671cb944566787c480
Albert Jané Medràn <109897647+R3dH4wk44@users.noreply.github.com>
2024-04-03 13:43:18 +0000

    adding the fibbonacci code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const getFibonacciNumber = position => {
+    let state = [0, 1];
+    const fibonacciNumberList = new Array(position)
+      .fill(0)
+      .map((fibonacciNumber, index) => {
+        if (index === 0) fibonacciNumber = state[0];
+        if (index === 1) fibonacciNumber = state[1];
+        if (index !== 0 && index !== 1) {
+          fibonacciNumber = state[0] + state[1];
+          state[0] = state[1];
+          state[1] = fibonacciNumber;
+        }
+        return fibonacciNumber;
+      });
+    const indexOfnumber = fibonacciNumberList.length - 1;
+    const fibonacciNumberForIndex =
+      fibonacciNumberList[fibonacciNumberList.length - 1];
+    return [indexOfnumber, fibonacciNumberForIndex];
+  };
+
+  const printFibonacciNumber = fibonacciNumber => {
+    console.log(
+      `The Fibonacci number for the position ${fibonacciNumber[0]} is: ${fibonacciNumber[1]}`
+    );
+  };
+
+  const main = () => {
+    const fibonacciNumber = getFibonacciNumber(Math.floor(Math.random() * 50));
+    printFibonacciNumber(fibonacciNumber);
+  };
+
+  main();
 };
