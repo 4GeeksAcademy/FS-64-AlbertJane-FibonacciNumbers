5ce540481978620bd74f934b6af457fbca676fc7
Albert Jané Medràn <109897647+R3dH4wk44@users.noreply.github.com>
2024-04-03 15:29:36 +0000

    Refactorizado, Readable & Mantenable

--- src/app.js
+++ src/app.js
@@ -2,22 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-import { eventListeners } from "@popperjs/core";
-
 window.onload = function() {
   const FIBONACCI_PLACEHOLDER = document.getElementById("fibonacci_text");
   const RANDOMIZE_BUTTON = document.getElementById("randomizer_button");
+  const main = () => {
+    const fibonacciNumber = getFibonacciNumber(
+      Math.floor(Math.random() * 50 + 1)
+    );
+    printFibonacciNumber(fibonacciNumber);
+    printOnClick();
+  };
 
   const getFibonacciNumber = position => {
     let state = [0, 1];
+
     const fibonacciNumberList = new Array(position)
       .fill(0)
       .map((fibonacciNumber, index) => {
-        if (index === 0) fibonacciNumber = state[0];
-        if (index === 1) fibonacciNumber = state[1];
-        if (index !== 0 && index !== 1) {
+        if (isEqual(index, 0)) fibonacciNumber = state[0];
+        if (isEqual(index, 1)) fibonacciNumber = state[1];
+        if (!isEqual(index, 0) && !isEqual(index, 1)) {
           fibonacciNumber = state[0] + state[1];
           state[0] = state[1];
           state[1] = fibonacciNumber;
@@ -31,8 +35,8 @@ window.onload = function() {
   };
 
   const printFibonacciNumber = fibonacciNumber => {
-    console.log("printing...");
-    const string = `The fibonacci number for the position ${fibonacciNumber[0]} is: <b> ${fibonacciNumber[1]} </b>`;
+    const string = `The fibonacci number for the position ${fibonacciNumber[0] +
+      1} is: <b> ${fibonacciNumber[1]} </b>`;
     FIBONACCI_PLACEHOLDER.innerHTML = string;
   };
 
@@ -44,14 +48,8 @@ window.onload = function() {
       printFibonacciNumber(fibonacciNumber);
     });
   };
-
-  const main = () => {
-    const fibonacciNumber = getFibonacciNumber(
-      Math.floor(Math.random() * 50 + 1)
-    );
-    printFibonacciNumber(fibonacciNumber);
-    printOnClick();
+  const isEqual = (comparationNumber, numberToCompare) => {
+    return comparationNumber === numberToCompare;
   };
-
   main();
 };
